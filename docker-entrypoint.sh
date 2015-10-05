221b0d636bcc9ee1dcfc9ba0f7d5bdc4a48cee34
Your Name <you@example.com>
2015-10-05 13:26:46 +1100

    Made server wait until database is up before running

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,18 +9,21 @@ set -x
     #      $DNS_ADDR'/dns.json'
 #fi
 
-
-dnsrecords=$(curl $DNS_ADDR'/dns.json')
-dnsrecords="${dnsrecords:1:${#dnsrecords}-2}"
-echo $dnsrecords
-
-#EXPORTS DNS SERVER ADDRESSES AS IP
-IFS=',' read -a array <<< "$dnsrecords"
-for element in "${array[@]}"
+while [ "$redisActive" != "true" ]
 do
-    element=${element/:/=}
-    setenv="export $element"
-    eval $setenv
+    dnsrecords=$(curl $DNS_ADDR'/dns.json')
+    dnsrecords="${dnsrecords:1:${#dnsrecords}-2}"
+    echo $dnsrecords
+
+    #EXPORTS DNS SERVER ADDRESSES AS IP
+    IFS=',' read -a array <<< "$dnsrecords"
+    for element in "${array[@]}"
+    do
+        element=${element/:/=}
+        setenv="export $element"
+        eval $setenv
+    done
+    sleep 5
 done
 
 #CONFIGURES THE NGINX FILE
